122b3f0b6f6a0af1ce610b6e89b9235e5008449e
FLOWZ <ketuimevbore@yahoo.com>
2019-10-12 17:46:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flowz/localizationapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.flowz.localizationapp;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+//    Button myButton1, myButton2;
+//    Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        myButton1 = findViewById(R.id.button1);
+//        myButton2 = findViewById(R.id.button2);
+
+
+//        myButton1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                fragment = new Fragment1();
+//                LoadFragment(fragment);
+//                }
+//        });
+//
+//        myButton2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//               fragment = new Fragment2();
+//                LoadFragment(fragment);
+//            }
+//        });
+
+    }
+
+//    public void LoadFragment (Fragment frag){
+//       getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, frag).commit();
+//
+//    }
+}
